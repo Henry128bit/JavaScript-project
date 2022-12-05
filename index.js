d6457de219b2cdd5434cfcd88e97a6de5bbbad70
Sergey Kamensky <henry.128bit@gmail.com>
2022-12-05 17:55:17 +0400

    Automating user questions with a loop

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt ('How many films have you already watched?', '');
 
 const personalMovieDB = {
@@ -8,12 +10,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt ('One of the last movies you watched?', ''),
-      b = +prompt ('How much would you rate it?', ''),
-      c = prompt ('One of the last movies you watched?', ''),
-      d = +prompt ('How much would you rate it?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt ('One of the last movies you watched?', ''),
+          b = +prompt ('How much would you rate it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Watched quite a few films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('You are a classic spectator');
+} else if (personalMovieDB.count >= 30) {
+    alert('You are a movie buff!');
+} else {
+    console.log('An error has occurred');
+}
 
 console.log(personalMovieDB);
